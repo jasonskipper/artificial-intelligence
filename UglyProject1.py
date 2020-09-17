41c6ff281848df226807e87ce280fb1a17e62d2d
jason skipper <52185268+jasonskipper@users.noreply.github.com>
2020-09-17 05:55:45 -0400

    Update UglyProject1.py

--- UglyProject1.py
+++ UglyProject1.py
@@ -20,7 +20,6 @@ BLUE = (0, 0, 255) # goal
 ORANGE = (255, 165, 0) # start 
 BLACK = (0, 0, 0) # barrier 
 WHITE = (255, 255, 255) # entry 
-YELLOW = (255, 255, 0)
 PURPLE = (128, 0, 128) # path 
 GREY = (128, 128, 128) # environment 
 
